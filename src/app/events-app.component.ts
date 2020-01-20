1d64530d99b80d0ce1b7e477fee8383a217eb67b
Liz Tiller <elizabeth.tiller@gmail.com>
2020-01-20 17:21:17 -0500

    first section

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h2>Hello World</h2>
+    <img src="/assets/images/angularconnect-shield.png" />
+  `,
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
