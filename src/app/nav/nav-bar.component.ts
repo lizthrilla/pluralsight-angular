b0427acc8414356024cf8e7237a339394c2fd9ae
Liz Tiller <elizabeth.tiller@gmail.com>
2020-01-22 15:15:02 -0500

    navbar

--- src/app/nav/nav-bar.component.ts
+++ src/app/nav/nav-bar.component.ts
@@ -3,8 +3,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-nav-bar',
-  // templateUrl: "./nav-bar.component.html"
-  template: `<div><h1>Test</h1></div>`
+  templateUrl: './nav-bar.component.html'
 })
 
 export class NavBarComponent {}
