22f2f5b0bab720b5b442e422b5824e81fa11edfd
Liz Tiller <elizabeth.tiller@gmail.com>
2020-01-22 15:48:45 -0500

    updated

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   template: `
     <app-nav-bar></app-nav-bar>
     <app-events-list></app-events-list>
   `
 })
 export class EventsAppComponent {
-  title = "ng-fundamentals";
+  title = 'ng-fundamentals';
 }
--- src/app/nav/nav-bar.component.ts
+++ src/app/nav/nav-bar.component.ts
@@ -3,7 +3,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html'
+  templateUrl: './nav-bar.component.html',
+  styles: [`
+    .nav.navbar-nav {font-size: 15px}
+  `]
 })
 
 export class NavBarComponent {}
